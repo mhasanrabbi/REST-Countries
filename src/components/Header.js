fa4dcd3c75925a358e1b9a5bc8ba145694a10e43
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2021-08-18 15:38:28 +0600

    dark mode with css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,23 @@
 import { Grid } from '@material-ui/core';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 
 function Header() {
+const [theme, setTheme] = useState('light-theme')
+
+const toggleTheme = () => {
+  if (theme === 'light-theme') {
+    setTheme('dark-theme')
+  }
+ else {
+  setTheme('light-theme')
+}
+}
+
+useEffect(() => {
+  document.documentElement.className = theme
+}, [theme])
 
   return (
       <Grid container 
@@ -19,7 +33,7 @@ function Header() {
             </h2>
           </Grid>
           <Grid item>
-            <button>Dark Mode</button>
+            <button onClick={toggleTheme}>Dark Mode</button>
           </Grid>
       </Grid>
   )
