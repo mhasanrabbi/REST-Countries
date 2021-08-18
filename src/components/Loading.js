fc6b85191ae2c9f757717904705e320658989101
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2021-08-18 12:53:26 +0600

    loading component

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,7 +1,15 @@
+import { CircularProgress } from '@material-ui/core';
 import React from "react";
 
+
 const Loading = () => {
-  return <div>Loading…</div>;
+  return (
+    <div className="loading-container">
+      <div className="loading">
+        <CircularProgress />
+      </div>
+    </div>
+  )
 };
 
 export default Loading;
